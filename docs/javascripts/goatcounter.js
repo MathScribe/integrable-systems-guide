bef98d7b1cf3c03e3a1184cface6484f9ecd0254
Deng Zixuan <zxdengmath@163.com>
2026-07-09 16:01:08 +0800

    Load GoatCounter script locally

--- docs/javascripts/goatcounter.js
+++ docs/javascripts/goatcounter.js
@@ -9,6 +9,12 @@
   var script = document.createElement("script");
   script.setAttribute("data-goatcounter", "https://nono6.goatcounter.com/count");
   script.setAttribute("async", "true");
-  script.setAttribute("src", "https://gc.zgo.at/count.js");
+
+  var currentScript = document.currentScript;
+  var localScriptUrl = currentScript && currentScript.src
+    ? new URL("goatcounter-count.js", currentScript.src).toString()
+    : "/integrable-systems-guide/javascripts/goatcounter-count.js";
+
+  script.setAttribute("src", localScriptUrl);
   document.head.appendChild(script);
 })();
